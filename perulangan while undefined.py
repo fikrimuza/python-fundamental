813661e6eed980010a2c0823a0f08ddba07aff5e
Fikri Muza <fikrimuzadi@gmail.com>
2022-12-31 23:21:00 +0700

    perulangan undefined

--- perulangan while undefined.py
+++ perulangan while undefined.py
@@ -2,25 +2,25 @@
 Program perulangan membaca buku dengan while
 '''
 
-jumlah_buku = 10
+book_count = 10
 print('Perintah ibu,"Baca semua bukumu"')
-total_jumlah_baca = 0
+read_count = 0
 
-jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
-print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
+understoof_count = 0
+print(f'Jumlah buku yang sudah dibaca dan dipahami {understoof_count}')
 
-while total_jumlah_baca < jumlah_buku * 2:
-    total_jumlah_baca = total_jumlah_baca + 1
-    if jumlah_buku_yang_sudah_dibaca_dan_dipahami == 5:
-        print(f'Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1} belum paham')
+while read_count < book_count * 2:
+    read_count = read_count + 1
+    if understoof_count == 5:
+        print(f'Buku ke {understoof_count + 1} belum paham')
     else:
-        jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
-        print(f'Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami')
+        understoof_count = understoof_count + 1
+        print(f'Buku ke {understoof_count} sudah dibaca dan dipahami')
 
-print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
+print(f'Jumlah buku yang sudah dibaca {understoof_count}')
 
-if jumlah_buku_yang_sudah_dibaca_dan_dipahami == jumlah_buku:
+if understoof_count == book_count:
     print('"Bu, seuma buku sudah di baca dan dipahami')
 else:
     print(f'"Bu, tidak semua buku bisa saya pahami,'
-          f' budi hanya memahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami} buku')
+          f' budi hanya memahami {understoof_count} buku')
